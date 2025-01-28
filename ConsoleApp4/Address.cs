86b5795e0811eba756ad632f7da12e79d47a96d8
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-01-28 22:04:31 +0200

    Add files via upload

---
+++ ConsoleApp4/Address.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Address
+    {
+        /// <summary>
+        /// индекс
+        /// </summary>
+        public int index = 3056;
+        /// <summary>
+        /// страна
+        /// </summary>       
+        public string country = "Makedonia";
+        /// <summary>
+        /// город
+        /// </summary>
+        public string city = "Parcho";
+        /// <summary>
+        /// улица
+        /// </summary>
+        public string street = "Vina";
+        /// <summary>
+        /// дом
+        /// </summary>
+        public int house = 17;
+        /// <summary>
+        /// аппартаменты
+        /// </summary>
+        public int appartamet = 21;
+        public void Print()
+        {
+            Console.WriteLine($"Индекс: {index}\nСтрана: {country}\nГород: {city}\nУлица: {street}\nДом: {house}\nАппартаменты: {appartamet}");
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address();
+            address.Print();
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
