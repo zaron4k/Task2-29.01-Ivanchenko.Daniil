3ba8be351cf3f97860bde7e20448b914ade47e38
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-01-28 21:48:56 +0200

    Add files via upload

---
+++ ConsoleApp3/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Person
+    {
+        /// <summary>
+        /// имя
+        /// </summary>
+        public string name = "Viktor";
+        /// <summary>
+        /// фамилия
+        /// </summary>       
+        public string surname = "Donald";
+        /// <summary>
+        /// возраст
+        /// </summary>
+        public int age = 33;
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {name}\nФамилия: {surname}\nВозраст: {age}");
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Print();
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
